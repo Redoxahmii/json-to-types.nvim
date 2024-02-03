1b45bb0326fe3a65c1deada585586872629b6c2e
Redoxahmii <fullmeta2334@gmail.com>
2024-02-03 22:34:13 +0500

    test(prod)
    hardcoded url to test

--- plugin/json-to-ts.lua
+++ plugin/json-to-ts.lua
@@ -1,8 +1,6 @@
--- INFO: This is a user command created you can go to command mode and type "MyFirstFunction" and it will print Hello passed in the local config.
-vim.api.nvim_create_user_command("ConvertToTs", require("json-to-ts").convertTypes, {})
+vim.api.nvim_create_user_command("ConvertJSONtoTS", require("json-to-ts").convertTypes, {})
 
--- INFO: This autocmd crreates a keymap for the json filetype.
--- ISSUE: The key map does not go away once you open a json file.
+-- INFO: This autocmd creates a keymap for the json filetype.
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "json", "jsonc" },
